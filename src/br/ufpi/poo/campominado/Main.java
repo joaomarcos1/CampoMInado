8c9acdd60e20a5cc28b97662b164b88e3117cf92
Alcemir R. Santos <alcemirsantos@gmail.com>
2018-05-29 18:17:03 -0300

    atualizei o Main

--- src/br/ufpi/poo/campominado/Main.java
+++ src/br/ufpi/poo/campominado/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 import br.ufpi.poo.campominado.enums.Acao;
 import br.ufpi.poo.campominado.exceptions.BombaExplodiuException;
+import br.ufpi.poo.campominado.exceptions.PosicaoInvalidaException;
 import br.ufpi.poo.campominado.model.CampoMinado;
 import br.ufpi.poo.campominado.model.Coordenada;
 import br.ufpi.poo.campominado.model.Jogada;
@@ -14,49 +15,78 @@ public class Main {
 
 	public static void main(String[] args) {
 		sc = new Scanner(System.in);
-		CampoMinado campo = new CampoMinado();
 
-		while (!campo.temVencedor()) {
-			Jogada umaJogada = null;
-			umaJogada = coletaJogada();
-			try {
-				campo.executa(umaJogada);
-			} catch (BombaExplodiuException b) {
-				System.out.println(b + "Você perdeu!\n\n"
-						+ campo.getTabuleiro());
+		while (true) {
+
+			CampoMinado campo = new CampoMinado();
+			campo.reseta();
+
+			while (!campo.temVencedor()) {
+				System.out.println(campo.getTabuleiro());
+
+				Jogada umaJogada = null;
+				umaJogada = coletaJogada();
+				try {
+					campo.executa(umaJogada);
+				} catch (BombaExplodiuException b) {
+					System.out.println("Você perdeu!" + b);
+					System.out.println(campo.getTabuleiro());
+					return;
+				} catch (PosicaoInvalidaException e) {
+					System.err
+							.println("ERROR: Jogada em posição inválida. Tente novamente!");
+				}
+			}
+
+			System.out.println("Você ganhou!\n\n" + campo.getTabuleiro());
+			if (!jogarNovamente()) {
+				sc.close();
 				return;
 			}
 		}
+	}
 
-		System.out.println("Você ganhou!\n\n" + campo.getTabuleiro());
-		sc.close();
+	private static boolean jogarNovamente() {
+		System.out.println("Deseja jogar novamente?(s/n) ");
+		String str = sc.next();
+		if (str.toLowerCase().equals("s"))
+			return true;
+
+		return false;
 	}
 
 	private static Jogada coletaJogada() {
-		System.out.println("\n::Escolha a ação \n\t(1= Investigar; 2=Marcar Bomba):");
+		System.out.println("\n::: Nova Jogada :::");
+		int opcao = 0;
+		while (opcao == 0) {
+			try {
+				System.out
+						.println("::Escolha a ação (1= Investigar; 2=Marcar Bomba):");
+				opcao = Integer.parseInt(sc.nextLine());
+			} catch (NumberFormatException e) {
+				System.err.println("Opção inválida!" + e);
+			}
+		}
 		Acao acao;
-		switch(Integer.parseInt(sc.nextLine())){
-		case 1:
-			acao = Acao.INVESTIGAR;
-			break;
+		switch (opcao) {
 		case 2:
-			acao= Acao.MARCAR;
+			acao = Acao.MARCAR;
 			break;
+		case 1:
 		default:
 			acao = Acao.INVESTIGAR;
+			break;
 		}
-		
-		System.out.println("\n::Digite posição da macação!");
+
 		int linha = 99, coluna = 99;
-		
-		while (linha == 99) {
-			System.out.print("Linha,Coluna:");
+		while (linha == 99 || coluna == 99) {
+			System.out.print("\n::Digite a posição assim: linha,coluna:");
 			try {
 				String[] entrada = sc.nextLine().split(",");
 				linha = Integer.parseInt(entrada[0]);
 				coluna = Integer.parseInt(entrada[1]);
 				break;
-			} catch (NumberFormatException e) {
+			} catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
 				System.err
 						.println("\nLinha inválida! Digite somente (um) número(s) separados por uma vírgula (,)!");
 			}
@@ -64,5 +94,4 @@ public class Main {
 
 		return new Jogada(acao, new Coordenada(linha, coluna));
 	}
-
 }
